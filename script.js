a05ebbfd512b3d941a095623cf40054868688577
John <john.26sfj@gmail.com>
2026-02-25 07:06:34 +0100

    testing first function, don´t understand error messages

---
+++ script.js
@@ -0,0 +1,51 @@
+const inputForm = document.querySelector("#input-form");
+const inputItem = document.querySelector("#input-item");
+const inputPrice = document.querySelector("#input-price");
+const submitBtn = document.querySelector("#submit-btn");
+
+let submittedExpenses = [];
+
+/*
+    Take input.
+    Take all three inputs, and create a <div> with each of them as a <p> beside each other, inside output-container. 
+    Push to submittedExpenses.
+    Store submittedExpenses in loaclStorage.
+*/
+
+// Popup error-message if input is empty on submit. Må innrømme at denne er bare lånt.
+const errorModal = (message) => {
+  const modal = document.createElement("dialog");
+  modal.classList.add("modal");
+
+  const errorElement = document.createElement("p");
+  errorElement.textContent = message;
+
+  const closeModalBtn = document.createElement("button");
+  closeModalBtn.textContent = "Ok";
+
+  modal.append(errorElement, closeModalBtn);
+  document.body.append(modal);
+
+  modal.showModal();
+  modal.addEventListener("click", () => {
+    modal.remove();
+  });
+};
+
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(inputForm);
+  const userInputItem = formData.get("input-item");
+  const userInputPrice = formData.get("input-price");
+  inputItem.value = "";
+  inputPrice.value = 0;
+  if (!userInputItem || !userInputPrice) {
+    return errorModal("Fields can´t be empty!");
+  }
+  submittedExpenses.push({
+    timestamp: new Date(),
+    itemname: userInputItem,
+    pricetag: userInputPrice,
+  });
+  console.log(submittedExpenses);
+});
